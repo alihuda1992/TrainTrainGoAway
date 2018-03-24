9abb29c621af4283a9ed7cde5f20e29f476c672f
Ali Huda <alihuda1992@gmail.com>
2018-03-24 17:41:09 -0400

    initialized firebase

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCB07gYJYRlOTxfdcPz3U_UfjTgoww4DQM",
+    authDomain: "trainscheduler-6612c.firebaseapp.com",
+    databaseURL: "https://trainscheduler-6612c.firebaseio.com",
+    projectId: "trainscheduler-6612c",
+    storageBucket: "",
+    messagingSenderId: "621289262559"
+  };
+  firebase.initializeApp(config);
+
 $(document).ready ({
 
 
@@ -6,7 +17,7 @@ $(document).ready ({
 
 
 
-    
+
 
 
 
